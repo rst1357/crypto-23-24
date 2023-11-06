73205c766a6f5724429d7bed4694cf700ff90bcf
YatsentiukAndrii <andyace-ipt25@lll.kpi.ua>
2023-11-06 20:55:40 +0200

    Added russian text check

--- cp3/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/func_lab3.py
+++ cp3/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/func_lab3.py
@@ -28,3 +28,74 @@ def Decode_Athene(a,b,letter,letters):
     x = letters.find(letter)
     index = (mod_inverse(a,m))*(x-b)%m
     return letters[index]
+
+
+def CountFreq(letter, text):
+    len_text = len(text)
+    freq = text.count(letter)/len_text
+    return freq
+
+def RussianCheck(text):  #works good only with large texts
+    alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+    alphabet_letter_frequencies = [
+        0.0801, 0.0159, 0.0454, 0.0165, 0.0298, 0.0849, 0.0072, 0.016, 0.0735,
+        0.0106, 0.0321, 0.0497, 0.0333, 0.067, 0.1097, 0.0281, 0.0473, 0.0547, 0.0626,
+        0.0262, 0.0026, 0.0097, 0.0048, 0.0144, 0.0073, 0.0061, 0.019, 0.0178,
+        0.0032, 0.0064, 0.0201
+    ]
+
+    freq = []
+    i = 0
+    while i < len(alphabet):
+        freq.append(CountFreq(alphabet[i],text))
+        i = i + 1
+
+    difference = 0
+    i = 0
+    while i < len(alphabet_letter_frequencies):
+        difference = difference + abs(alphabet_letter_frequencies[i] - freq[i])
+        i = i + 1
+
+    if difference < 0.15:
+        return True
+    else:
+        return False
+
+def ReadText(filename, encoding):
+    with open(filename, 'r', encoding=encoding) as file:
+        file_contents = file.read()
+        file_contents = file_contents.lower()
+
+    file_contents = file_contents.replace(",", "")
+    file_contents = file_contents.replace("!", "")
+    file_contents = file_contents.replace("?", "")
+    file_contents = file_contents.replace("=", "")
+    file_contents = file_contents.replace("-", "")
+    file_contents = file_contents.replace("", "")
+    file_contents = file_contents.replace(".", "")
+    file_contents = file_contents.replace(":", "")
+    file_contents = file_contents.replace(";", "")
+    file_contents = file_contents.replace("1", "")
+    file_contents = file_contents.replace("2", "")
+    file_contents = file_contents.replace("3", "")
+    file_contents = file_contents.replace("4", "")
+    file_contents = file_contents.replace("5", "")
+    file_contents = file_contents.replace("6", "")
+    file_contents = file_contents.replace("7", "")
+    file_contents = file_contents.replace("8", "")
+    file_contents = file_contents.replace("9", "")
+    file_contents = file_contents.replace("0", "")
+    file_contents = file_contents.replace(" ", "")
+    file_contents = file_contents.replace("'", "")
+    file_contents = file_contents.replace("\n", "")
+    file_contents = file_contents.replace("ъ", "ь")
+    file_contents = file_contents.replace("ё", "е")
+    file_contents = file_contents.replace("«", "")
+    file_contents = file_contents.replace("»", "")
+    file_contents = file_contents.replace("…","")
+    file_contents = file_contents.replace("„", "")
+    file_contents = file_contents.replace("“", "")
+    file_contents = file_contents.replace("—", "")
+    file_contents = file_contents.replace("*", "")
+    return file_contents
+
