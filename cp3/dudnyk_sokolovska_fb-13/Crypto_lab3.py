854df4dc1672541e1fe0c9a6d9f15daf83e6f5e9
darsi-ai <dariia.s707@gmail.com>
2023-11-28 15:58:15 +0200

    дозагругризили код

--- cp3/dudnyk_sokolovska_fb-13/Crypto_lab3.py
+++ cp3/dudnyk_sokolovska_fb-13/Crypto_lab3.py
@@ -3,8 +3,11 @@ from math import gcd
 from typing import List
 
 frequent_bigrams = ["ст", "но", "то", "на", "ен"]
-alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
-#           01234567890123456789012345678901c
+# ["то", "он", "на", "не", "по"]
+alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя"
+#           0123456789012345678901234567890 -> 31
+non_tipical_bigrams = ["оь", "йц", "йш", "ъй", "йь", "йю", "йя", "щй", "ьй", "эь", "ыа", "щю"]
+
 
 # from lab1
 
@@ -32,9 +35,10 @@ def solve_linear_mod_expression(a:int, b:int, modulo:int):
 
     """ розв'язання рівнянь виду ax = b mod c """
 
+    res = []
     if gcd(a , modulo) == 1:
-        res = pow(a,-1,modulo) * b % modulo
-        print(f"Рівняння {a}x = {b} mod({modulo}) : x =", res)
+        res.append(pow(a,-1,modulo) * b % modulo)
+        # print(f"Рівняння {a}x = {b} mod({modulo}) : x =", res)
         return res
     elif gcd(a, modulo) > 1:
         d = gcd(a, modulo)
@@ -45,48 +49,110 @@ def solve_linear_mod_expression(a:int, b:int, modulo:int):
         else:
             x = pow(a // d, -1, modulo // d) * (b // d) % (modulo // d)
             res = [x + (modulo // d) * i for i in range(d)]  # усі можливі корені
-            print(f"Усі можливі корені для рівняння {a}x = {b} mod({modulo}) : ", res)
+            # print(f"Усі можливі корені для рівняння {a}x = {b} mod({modulo}) : ", res)
             return res
 
-def get_X(X:List[str], alphabet:str = alphabet):
+def check_text(text:str, bigrams:List[str] = non_tipical_bigrams):
+
+    """перевіряємо на наявність в тексті неможливих поєднань біграм"""
+
+    if any(bigram in text for bigram in bigrams):
+        return False
+    return True
+
+
+def encode_to_num(bigrams:List[str], alphabet:str = alphabet):
+
+    """X = x[i] * m + x[i+1], де m це довжина алфавіту - нумерування біграм"""
+
     numsX = []
-    for x in X:
-        a = x[0]
-        b = x[1]
+    for bigram in bigrams:
+        a = bigram[0]
+        b = bigram[1]
         numsX.append(alphabet.index(a) * len(alphabet) + alphabet.index(b))
     return numsX
 
-def get_Y(X:List[int], Y:List[int], alphabet:str = alphabet):
-    numsY = []
-    for y in Y:
-        a = y[0]
-        b = y[1]
-        if gcd(alphabet.index(a), len(alphabet)) == 1 and (alphabet.index(b) in range(0, len(alphabet)+1)): pass
-        numsY.append((alphabet.index(a) * X + alphabet.index(b)) % len(alphabet)^2)
-    return numsY
+def decode_to_bigram(bigrams_nums:List[int], alphabet:str = alphabet):
 
-# def get_key_options(X: List[str], Y: List[str], alphabet:str = alphabet): 
-#     """ передаємо наші біграми, X: найчастіші 5 біграм у нормальному тексті, Y - у шифротексті, і знаходимо можливі коефіцієнти для дешифрування """
-#     numsX = []
-#     numsY = []
-#     # for x in X:
+    """конвертувати біграми з циферного у буквенний вигляд"""
+
+    bigrams  = []
+    for bigram in bigrams_nums:
+        l1 = alphabet[bigram // len(alphabet)]
+        l2 = alphabet[bigram % len(alphabet)]
+
+        decoded_bigram = l1+l2
+        bigrams.append(decoded_bigram)
+
+    return bigrams
+
+def get_keys(text:str, frequent_bigrams:List[str] = frequent_bigrams, alphabet:str = alphabet):
+
+    """шукаємо можливі ключі"""
+
+    # найчастіші біграми шифртексту
+    sorted_values = sorted(frequency_bigrams(text).items(), key=lambda x: x[1], reverse=True)
+    top_5_encrypted = list(dict(sorted_values[:5]).keys())
+
+    mod = len(alphabet)**2
+
+    X = encode_to_num(frequent_bigrams)
+    Y = encode_to_num(top_5_encrypted)
+
+    X1 = X[0]
+    X2 = X[1]
+
+    Y1 = Y[0]
+    Y2 = Y[1]
+
+    keys_a = []
+    keys_b = []
+    if X1 != X2 and Y1 != Y2:
+        keys_a = list(solve_linear_mod_expression((X1-X2)%mod, (Y1-Y2)%mod, mod))
+        for a in keys_a:
+            if a != 0:
+                keys_b.append((Y1 - a*X1) % mod)
+            else: 
+                keys_b.add(None)
+        print(f'Можливі a: {keys_a}')
+        print(f'Можливі b: {keys_b}')
+    return keys_a, keys_b
 
 
+def decipher(text:str, alphabet:str=alphabet): 
 
-# # тестування розв'язку рівнянь - робе
-# solve_linear_mod_expression(3, 8, 9) # нема коренів
-# solve_linear_mod_expression(2, 5, 7)
-# solve_linear_mod_expression(4, 3, 11)
-# solve_linear_mod_expression(7, 9, 13)
-# solve_linear_mod_expression(9, 12, 17)
-# solve_linear_mod_expression(6, 10, 14)
+    """розшифровуємо текст: Xi = a^(-1) * (Yi - b) mod (len(alphabet))^2 і повертаємо файл з результатом якзо текст змістовний"""
 
+    final_text = ""
+    decoded_nums = []
+    mod = len(alphabet)**2
+    a_keys, b_keys = get_keys(text)
+    numerated_bigrams = encode_to_num(list(frequency_bigrams(text).keys()))
+
+    for Y in numerated_bigrams: 
+        for a in a_keys:
+            for b in b_keys:
+                decoded_nums.append(solve_linear_mod_expression(a, Y-b, mod))
+    
+    decoded_strings = []
+    for solutions in decoded_nums:
+        for solution in solutions:
+            if isinstance(solution, int):  # Перевіряємо, чи solution є цілим числом
+                solution = [solution]  # Якщо так, створюємо список з одним цілим числом
+            decoded_strings.append(decode_to_bigram(solution))
+
+    final_text = ''.join([''.join(decoded_string) for decoded_string in decoded_strings])
+
+    if check_text(final_text): 
+        with open("decrypted_text.txt", "w", encoding="utf-8") as file:
+            file.write(final_text)
+    else:
+        print("Текст не змістовний, файлу не створено")
+
+  
 # пошук найчастіших біграм у шифротексті
-test_text = process_text("01_utf8.txt")
-sorted_values = sorted(frequency_bigrams(test_text).items(), key=lambda x: x[1], reverse=True)
-top_5_values = dict(sorted_values[:5])
-print(top_5_values)
+test_text = process_text("V1_for_test_utf8.txt")
+# get_keys(test_text)
 
-# print(get_X(frequent_bigrams))
-# print(get_Y(top_5_values))
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+decipher(test_text)
+# не виходить змістовний текст
