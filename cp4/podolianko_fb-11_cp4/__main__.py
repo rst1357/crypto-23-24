5af5db17a0b2c24a4a3d236e896bb3041d726c8c
timofey282228 <tymopod-ipt25@lll.kpi.ua>
2023-12-14 14:14:33 +0200

    handmade gcd, hpow; nonprime logging; formatting unified

--- cp4/podolianko_fb-11_cp4/__main__.py
+++ cp4/podolianko_fb-11_cp4/__main__.py
@@ -1,5 +1,7 @@
-import random
 from rsa import *
+import random
+import logging
+logging.basicConfig(level=logging.INFO)
 
 A_keypair = GenerateKeyPair(256)
 B_keypair = GenerateKeyPair(256)
@@ -11,22 +13,22 @@ while A_keypair[0][1] > B_keypair[0][1]:
 print(
     f"""
 A's keypair:
-    Public: ({A_keypair[0][0]:#X},
-        {A_keypair[0][1]:#X})
-    Private: ({A_keypair[1][0]:#X},
-        {A_keypair[1][1]:#X},
-        {A_keypair[1][2]:#X})
+    Public: ({A_keypair[0][0]:0X},
+        {A_keypair[0][1]:0X})
+    Private: ({A_keypair[1][0]:0X},
+        {A_keypair[1][1]:0X},
+        {A_keypair[1][2]:0X})
     """
 )
 
 print(
     f"""
 B's keypair:
-    Public: ({B_keypair[0][0]:#X},
-        {B_keypair[0][1]:#X})
-    Private: ({B_keypair[1][0]:#X},
-        {B_keypair[1][1]:#X},
-        {B_keypair[1][2]:#X})
+    Public: ({B_keypair[0][0]:0X},
+        {B_keypair[0][1]:0X})
+    Private: ({B_keypair[1][0]:0X},
+        {B_keypair[1][1]:0X},
+        {B_keypair[1][2]:0X})
     """
 )
 
@@ -47,31 +49,31 @@ b_received_key_from_a = ReceiveKey(a_protected_key_for_b, B_keypair[1], A_keypai
 
 print(
     f"""
-Message: {message: #x}
+Message: {message: 0X}
 Encryption:
-    B encrypts for A: {for_a_encrypted_message: #x}
-    A encrypts for B: {for_b_encrypted_message: #x}
+    B encrypts for A: {for_a_encrypted_message: 0X}
+    A encrypts for B: {for_b_encrypted_message: 0X}
 
 Decryption:
-    Of message to A: {for_a_decrypted_message: #x}
+    Of message to A: {for_a_decrypted_message: 0X}
     (result matched to original? {for_a_decrypted_message == message})
 
-    Of message to B:{for_b_decrypted_message: #x}
+    Of message to B:{for_b_decrypted_message: 0X}
     (result matched to original? {for_b_decrypted_message == message})
     
 Signing and validating signatures (of the given message):
-    A's signature: {by_a_signed_message[1]: #x}
+    A's signature: {by_a_signed_message[1]: 0X}
         (verified? {Verify(by_a_signed_message, A_keypair[0])})
-    B's signature: {by_b_signed_message[1]: #x}
+    B's signature: {by_b_signed_message[1]: 0X}
         (verified? {Verify(by_b_signed_message, B_keypair[0])})
         
 Simulating secure key transfer protocol
-    Key to be sent: {k:#X}
+    Key to be sent: {k:0X}
     A -> B:
-        (k1, S1): ({a_protected_key_for_b[0]:#X}, 
-            {a_protected_key_for_b[1]:#X})
+        (k1, S1): ({a_protected_key_for_b[0]:0X}, 
+            {a_protected_key_for_b[1]:0X})
             
-        B received (and verified) key: {b_received_key_from_a:#X}
+        B received (and verified) key: {b_received_key_from_a:0X}
     """
 )
 
