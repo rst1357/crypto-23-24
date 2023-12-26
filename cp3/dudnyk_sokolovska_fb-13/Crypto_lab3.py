037817663b1f95cc0149d54d83c1b3e3a1375c61
darsi-ai <dariia.s707@gmail.com>
2023-12-26 16:48:13 +0200

    оновили функцію пошуку ключів та дешифрування

--- cp3/dudnyk_sokolovska_fb-13/Crypto_lab3.py
+++ cp3/dudnyk_sokolovska_fb-13/Crypto_lab3.py
@@ -31,8 +31,19 @@ def process_text(file_name:str):
     return text
 
 # 2
-# for invertion: x^(-1) = pow(x, -1, modulo)
 # for gcd: gcd(a, b)
+
+def check_reversed_exist(num:int, modulo:int):
+
+    """ шукаємо обернений елемент """
+
+    if gcd(num, modulo) == 1: 
+        for i in range(1, modulo):
+            if (num * i) % modulo == 1:
+                return i
+        
+    return None
+
 
 def solve_linear_mod_expression(a:int, b:int, modulo:int):
 
@@ -89,83 +100,85 @@ def decode_to_bigram(bigrams_nums:List[int], alphabet:str = alphabet):
 
     return bigrams
 
-def get_keys(text:str, frequent_bigrams:List[str] = frequent_bigrams, alphabet:str = alphabet):
+def get_keys(text:str, frequent_bigrams:List[str] =frequent_bigrams, alphabet:str = alphabet):
 
     """шукаємо можливі ключі"""
-    sorted_values = sorted(frequency_bigrams(text).items(), key=lambda x: x[1], reverse=True)
-    top_5_encrypted = []
-    count = 0
-    temp_values = []
-    # top_5_encrypted = list(dict(sorted_values[:5]).keys())
-    for key, val in sorted_values:
-        if val not in temp_values and count < 5:
-            temp_values.append(val)
-            top_5_encrypted.append(key)
-            count += 1
 
     mod = len(alphabet)**2
-
+    sorted_encrypted = sorted(frequency_bigrams(text).items(), key=lambda x: x[1], reverse=True)
+    top_encrypted = list(dict(sorted_encrypted[:5]).keys())
+    # print(top_encrypted)
+    # print(frequent_bigrams)
     X = encode_to_num(frequent_bigrams)
-    Y = encode_to_num(top_5_encrypted)
+    Y = encode_to_num(top_encrypted)
 
-    keys_b = []
-    keys = defaultdict(int)
-
-    # Generate combinations of indices of X and Y
     X_combinations = list(combinations(range(len(X)), 2))
     Y_combinations = list(combinations(range(len(Y)), 2))
+    # print(X)
+    # print(Y)
 
-    for x1, x2 in X_combinations:
-        for y1, y2 in Y_combinations:
-            if pow(x1 - x2, -1, mod) is not None:
-                a_values = solve_linear_mod_expression((X[x1]-X[x2])%mod, (Y[y1]-Y[y2])%mod, mod)
-                for a in a_values:
-                    if a != 0:
-                        b = (Y[y1] - a*X[x1]) % mod
-                        keys_b.append(b)
-                keys[a] = keys_b
+    keys = defaultdict(int)
 
+    for x in X_combinations:
+        for y in Y_combinations:
+            x1 = X[x[0]]
+            x2 = X[x[1]]
+            y1 = Y[y[0]]
+            y2 = Y[y[1]]
+
+            if x1 == y1 or x2 == y2 : continue
+            # print(x1, x2, y1, y2)
+
+            a_keys = solve_linear_mod_expression(x1 - x2, y1 - y2, mod)
+            # print("a = ", a_keys, ", x1 = ", x1, ", x2 = ", x2, ", y1 = ", y1, ", y2 = ",  y2)
+            b_keys = []
+            for a in a_keys:
+                b = (y1 - a*x1) % mod
+                b_keys.append(b)
+            keys[a] = b_keys
     # print(keys)
     return keys
-
+            
 
 def decipher(text: str, alphabet: str = alphabet):
-    """ розшифровуємо текст"""
-    mod = len(alphabet) ** 2
-    possible_keys = get_keys(text)
-    numerated_bigrams = encode_to_num(list(frequency_bigrams(text).keys()))
 
-    for a, b_vals in possible_keys.items():
+    """ розшифровуємо текст"""
+
+    mod = len(alphabet)**2
+    keys = get_keys(text)
+    numerated_bigrams = encode_to_num(list(frequency_bigrams(text).keys()))
+    
+    for a, b_vals in keys.items():
         for b in b_vals:
-            if pow(a, -1, mod) is None:
-                continue
+            if check_reversed_exist(a, mod) is None: continue
+
             decrypted_nums = []
+
             for y in numerated_bigrams:
                 x = (pow(a, -1, mod) * (y - b)) % mod
                 decrypted_nums.append(x)
-            decrypted_text = ''
-            for n in decrypted_nums:
-                l1 = n // len(alphabet)
-                l2 = n % len(alphabet)
-                bigram = alphabet[l1] + alphabet[l2]
-                decrypted_text += bigram
+            # print(decrypted_nums)
 
-            if check_text(decrypted_text):
-                print("Текст змістовний, створено файл")
-                print(f"Ключі: a = {a}, b = {b}")
+                decrypted_text = ''
 
-                with open("decrypted.txt", "w", encoding="utf-8") as file:
-                    file.write(decrypted_text)
-                return 
-    print("Не вдалося знайти змістовний текст")
+                for num in decrypted_nums:
+                    l1 = num // len(alphabet)
+                    l2 = num % len(alphabet)
+                    bigram = alphabet[l1] + alphabet[l2]
+                    decrypted_text += bigram
+
+                if check_text(decrypted_text):
+                    print("Текст змістовний, створено файл")
+                    print(f"Ключі: a = {a}, b = {b}")
+
+                    with open("decrypted.txt", "w", encoding="utf-8") as file:
+                        file.write(decrypted_text)
+                        return
+    print("Не вдалося знайти змістовний текст") 
+
+# (13, 151)
 
 
-
-
-
-
-
-        
 test_text = process_text("V1_for_test_utf8.txt")
 # get_keys(test_text)
 
