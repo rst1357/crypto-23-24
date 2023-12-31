f8a3666bf59f5ac837d90a83ef10900be68e1771
YatsentiukAndrii <andyace-ipt25@lll.kpi.ua>
2023-12-31 18:28:34 +0200

    Added Routine

--- cp4/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/main.py
+++ cp4/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/main.py
@@ -2,9 +2,15 @@ import random
 import secrets
 import math
 import string
-import hashlib
+import webbrowser
 
+def open_webpage_with_web_browser(url):
+    webbrowser.open(url)
 
+def decimal_to_hex(decimal_num):
+    decimal_num = int(decimal_num)
+    res = hex(decimal_num)
+    return res[2:].upper()
 
 def generate_random_string(length):
     characters = string.ascii_letters + string.digits
@@ -56,9 +62,9 @@ def is_prime_miller_rabin(num, k=5):
 
 
 def GetPrime():
-    random_number = secrets.randbits(512)
+    random_number = secrets.randbits(256)
     while not is_prime_miller_rabin(random_number):
-        random_number = secrets.randbits(512)
+        random_number = secrets.randbits(256)
     return random_number
 def _get_pq():
     num1 = GetPrime()
@@ -119,6 +125,19 @@ def Generate_Keys():
     n2 = PQ_array[1][0] * PQ_array[1][1]
     return [[e1,n1,d1],[e2,n2,d2]]
 
+def from_number_to_string(num):
+    decoded_message_num = num
+    decoded_message_num_without_first = str(decoded_message_num)[1:]
+    decoded_message = ""
+    i = 0
+    while i < len(decoded_message_num_without_first) and decoded_message_num_without_first[i:i+3] != '':
+        batch = int(decoded_message_num_without_first[i:i+3])
+        char = chr(batch)
+        decoded_message = decoded_message + char
+        i = i + 3
+
+    return decoded_message
+
 def Encrypt(message, n, e): # n and e are public keys. Message will be a string without numbers. (n = p*q, e is coprime of (p-1)*(q-1))
     numbers = [ord(char) for char in message]
     message = ""
@@ -130,9 +149,10 @@ def Encrypt(message, n, e): # n and e are public keys. Message will be a string 
         else:
             temp = str(num)
             message = message + temp
-    message = str(random.randint(1,9)) + message # to make possible 0XX code at first pos, delete first number when decode
+    message = str(1) + message
+    #message = str(random.randint(1,9)) + message # to make possible 0XX code at first pos, delete first number when decode
     encoded_message = pow(int(message), e, n)
-    return encoded_message
+    return [encoded_message, int(message)]
 
 def Decrypt(message, n, d):
     decoded_message_num = pow(message, d, n)
@@ -148,50 +168,84 @@ def Decrypt(message, n, d):
     return decoded_message
 
 #signatures
-def generate_signature(data, your_signature):
-    return hashlib.sha256((data + your_signature).encode()).digest() #generates a hash out of a string
-
-def encrypt_signature(signature, n1, e1):
-    hex_string = ''.join(['{:02X}'.format(byte) for byte in signature])
-    return Encrypt(hex_string, n1, e1)
-
-def check_signature(encrypted_signature, encrypted_text, n1, d1, your_signature):
-    signature =  bytes.fromhex(Decrypt(encrypted_signature, n1, d1))
-    return signature == generate_signature(Decrypt(encrypted_text, n1, d1), your_signature)
+def generate_signature(message, n1, d1): #A needs his public part: n, and private part:d  to send somebody a signature
+    signature = pow(message, d1, n1)
+    return signature
 
 
+def check_signature(message, signature, n1, e1):  #B needs A's public parts: n, e, to check signature
+    return message == pow(signature, e1, n1)
 
 
+def send_key(n2, e2, n1, d1):  #A needs B's public parts: e,n and A's private part: d and A's public part: n, to send a key
+    key_init = random.randint(1,5087)
+    key = pow(key_init, e2, n2)
+    sign = generate_signature(key, n1, d1)
+    sign = pow(sign, e2, n2)
+    return [key, sign]
 
-#sender side
-
-keys = Generate_Keys()
-e1 = keys[0][0] #public key
-n1 = keys[0][1] #public key
-d1 = keys[0][2] #private key
-message = "Hello world, that's my real RSA encrypted message!"
-signature_string = "MY_SIGNATURE"
-encrypted = Encrypt(message, n1, e1)
-signature = generate_signature(message, signature_string)
-encrypted_signature = encrypt_signature(signature, n1, e1)
-#signature + encrypted is getting sent
-
-#recipient side
-#key is already known
-decrypted = Decrypt(encrypted, n1, d1)
-if check_signature(encrypted_signature, encrypted, n1, d1, "MY_SIGNATURE"):
-    print("Signature is verified. Message is valid.")
-    print(f"Message {decrypted}")
-    print(f"Signature : {signature_string}")
-    print(f"Public keys :")
-    print(f"n: {n1}")
-    print(f"e: {e1}")
-    print(f"Private key : d: {d1}")
-
-else:
-    print("SOMEONE CHANGED THE MESSAGE!!!! SIGNATURE IS NOT VERIFIED!!!!")
+def receive_key(key, sign, n2, d2, e1, n1): #B needs B's private part: d and B's public part: n,
+    key_check = pow(key, d2, n2)
+    sign_check = pow(sign, d2, n2)
+    return check_signature(key, sign_check, n1, e1)
 
 
 
 
+
+
+
+def Routine():
+    open_webpage_with_web_browser("http://asymcryptwebservice.appspot.com/?section=rsa")
+    print("Enter 256 in 'Key size' space")
+    server_modulus = input("Insert generated modulus here: ")
+    server_exponent = input("Insert generated exponent here: ")
+    server_modulus = int(server_modulus, 16)
+    server_exponent = int(server_exponent, 16)
+    print("*"*200)
+    print("Press 'Encryption' button on the right side")
+    print("Test message will be : 'Test message RSA'")
+    print("Insert a server given modulus into 'Modulus' space")
+    print("Insert a server given exponent into 'Public exponent' space")
+    print(f"Insert this translated test message into 'Message': {decimal_to_hex(Encrypt('Test message RSA', server_modulus, server_exponent)[1])}")
+    encoded_message_hex = input("Insert here generated encrypted message: ")
+    encoded_message = int(encoded_message_hex, 16)
+    print(f"Message encrypted locally: {decimal_to_hex(Encrypt('Test message RSA', server_modulus, server_exponent)[0])}")
+    print(f"Are locally encrypted and server encrypted the same: {decimal_to_hex(Encrypt('Test message RSA', server_modulus, server_exponent)[0]) == encoded_message_hex[1:]}")
+    print("*" * 200)
+    print("Press 'Decryption' button on the left side")
+    print("Insert into 'Ciphertext' message that was encrypted locally")
+    server_decoded = input("Insert here from 'Message' space: ")
+    print(f"Are locally decoded and server decoded messages the same: {decimal_to_hex(Encrypt('Test message RSA', server_modulus, server_exponent)[1]) == server_decoded}")
+    print("*" * 200)
+    print("Press 'Signature' button on the left side")
+    print("Insert this translated test message into 'Message': BDE4C67958FC0264B93FC871F4A84B5132FE8CF9")
+    server_generated_signature = input("Insert from 'Signature' space here: ")
+    print(f"Locally checked signature result: {check_signature(int('BDE4C67958FC0264B93FC871F4A84B5132FE8CF9', 16), int(server_generated_signature, 16), server_modulus, server_exponent)}")
+    print("*" * 200)
+    print("Press 'Verification' button on the left side")
+    print("Locally generated keys:")
+    keys = Generate_Keys()
+    print(f"Modulus : {decimal_to_hex(keys[0][1])}")
+    print(f"Public exponent: {decimal_to_hex(keys[0][0])}")
+    print(f"Private key: {decimal_to_hex(keys[0][2])}")
+    message = Encrypt('Test message RSA', keys[0][1], keys[0][0])[1]
+    print(f"Translated message: {decimal_to_hex(Encrypt('Test message RSA', keys[0][1], keys[0][0])[1])}")
+    print("Generating a signature...")
+    print(f"Locally generated Signature: {decimal_to_hex(generate_signature(message, keys[0][1], keys[0][2]))}")
+    print("Insert Translated message into 'Message' space")
+    print("Insert Locally generated Signature into 'Signature' space")
+    print("Insert Modulus into 'Modulus' space")
+    print("Insert Public exponent into 'Public exponent' space")
+    print("*" * 200)
+    print("Press 'Send key' button on the left side")
+    print(f"Insert this modulus into 'Modulus' space: {decimal_to_hex(keys[0][1])}")
+    print(f"Insert this public exponent into 'Public exponent': {decimal_to_hex(keys[0][0])}")
+    server_key_hex = input(f"Insert here from 'Key' space: ")
+    server_signature_hex = input(f"Insert here from 'Signature' space: ")
+    server_key = int(server_key_hex, 16)
+    server_signature = int(server_signature_hex, 16)
+    result = receive_key(server_key, server_signature, keys[0][1], keys[0][2], server_exponent, server_modulus) #Maybe used wrong parameters??
+    print(f"Is key received succ: {result}")
+Routine()
 
