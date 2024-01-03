0ee26134f56fa6b3bd7d988177b19f0607184048
darsi-ai <dariia.s707@gmail.com>
2024-01-03 14:54:26 +0200

    загрузили протокол

--- cp4/dudnyk_sokolovska_fb-13/Crypto_lab4.py
+++ cp4/dudnyk_sokolovska_fb-13/Crypto_lab4.py
@@ -112,12 +112,12 @@ def get_keys_pairs(bits:int=256):
 
     return secret_keys, open_keys
 
-# print(get_keys_pairs()) 
+# print("get_key_pairs: ", get_keys_pairs()) 
 
 def generate_pairs_AB():
     A_secretKeys, A_openKeys = get_keys_pairs()
     # перевірка q1*p1 <= q2*p2
-    # міняємо
+    # міняємо якщо не задовольняється умова
     B_secretKeys, B_openKeys = get_keys_pairs()
     if B_secretKeys['q'] * B_secretKeys['p'] > A_secretKeys['q'] * A_secretKeys['p']: 
         A_secretKeys, B_secretKeys = B_secretKeys, A_secretKeys
@@ -136,12 +136,9 @@ def cipher_message(M: int, openKeys: dict):
     res = horner_pow(M, e, n)
     return res
 
-def decipher_message(C: int, privateKeys: dict=None, openKeys: dict = None):
-    if privateKeys is not None:
-        p, q, d = privateKeys['p'], privateKeys['q'], privateKeys['d']
-        n = p * q
-    else :
-        n = openKeys['n']
+def decipher_message(C: int, privateKeys: dict):
+    p, q, d = privateKeys['p'], privateKeys['q'], privateKeys['d']
+    n = p * q
         
     if C >= n:
         raise ValueError(f"Криптограма завелика для розшифрування!\nКриптограма: {C}\nn: {n}")
@@ -161,15 +158,16 @@ def send_message(M:int, A_privateKeys:dict, B_openKeys:dict):
     encrypted_M = cipher_message(M, B_openKeys)
     S = crypto_sign(M, A_privateKeys)
     S1 = horner_pow(S, B_openKeys['e'], B_openKeys['n'])
-
-    print("Підписанне повідомлення відправлено успішно!")
+    if encrypted_M and S and S1:
+        print("Підписанне повідомлення відправлено успішно!")
 
     return encrypted_M, S1
 
 def receive_message(encrypted_M:int, S1:int, B_privateKeys:dict, A_openKeys:dict):
     M = decipher_message(encrypted_M, B_privateKeys)
-    S = decipher_message(S1, B_privateKeys)
-
+    d = B_privateKeys['d']
+    n = B_privateKeys['p'] * B_privateKeys['q']
+    S = horner_pow(S1, d, n)
     verified = verification(M, S, A_openKeys)
 
     if not verified:
@@ -177,38 +175,55 @@ def receive_message(encrypted_M:int, S1:int, B_privateKeys:dict, A_openKeys:dict
     else:
         print("Аутентифікацію за підписом пройдено успішно!\nОтримане повідомлення:", M)
 
-# messageA = random.randint(1, 130)
-# messageB = random.randint(131, 260)
-# print("messageA:", messageA, f"(length={sys.getsizeof(messageA)})\nmessageB:", messageB, f"(length={sys.getsizeof(messageB)})")
+messageA = random.randint(1, 130)       # задаємо різні проміжки щою упевнитися в правильній роботі
+messageB = random.randint(131, 260)
+print("messageA:", messageA, f"(length={sys.getsizeof(messageA)})\nmessageB:", messageB, f"(length={sys.getsizeof(messageB)})")
+A_openKeys, A_secretKeys, B_openKeys, B_secretKeys = generate_pairs_AB()
 
-# A_openKeys, A_secretKeys, B_openKeys, B_secretKeys = generate_pairs_AB()
+print(f"\nsending message {messageA} to B")
+encrypted_A, s1a = send_message(messageA, A_secretKeys, B_openKeys)
 
-# messageA_encrypted, messageB_encrypted = cipher_message(messageA, B_openKeys), cipher_message(messageB, A_openKeys)
-# print("encrypted message for A: ", messageA_encrypted, "\nencrypted message for B: ", messageB_encrypted)
+print(f"\nsending message {messageB} to A")
+encrypted_B, s1b = send_message(messageB, B_secretKeys, A_openKeys)
 
-# messageA_original, messageB_original = decipher_message(messageA_encrypted, B_secretKeys), decipher_message(messageB_encrypted, A_secretKeys)
-# print("decrypted message for A: ", messageA_original, "\ndecrypted message for B: ", messageB_original)
+print("\nreceiving message from A:")
+receive_message(encrypted_A, s1a, B_secretKeys, A_openKeys)
 
-# encryptedA , sign1= send_message(messageA, A_secretKeys, B_openKeys)
-# receive_message(encryptedA, sign1, B_secretKeys, A_openKeys)
-
-# encryptedB, sign2 = send_message(messageB, B_secretKeys, A_openKeys)
-# receive_message(encryptedB, sign2, A_secretKeys, B_openKeys)
+print("\nreceiving message from B:")
+receive_message(encrypted_B, s1b, A_secretKeys, B_openKeys)
 
 
-# статичні дані для перевірки сервером
-# 128 bits для спрощення перевірки
 
-static_open_keys = {'n' : 77290010257482367667872392744139331143985885480088616497010126116309693800311,
-                    'e' : E}
+# статичні дані для перевірки
 
-static_secret_keys = {'p': 293295450714564884956358051352919305107, 'q': 263522704048693210092176810079307682573, 
-                      'd': 38584269429391803454381205630559935563415609437152597019042333049816532366585}
+# A_open_static = {'n': 2473737842202304934787222987836508747179338223921489399061250469701340947202938217684161015132617360930669790884492247351288673941655064409712301120118031, 
+#                  'e': 65537}
+# A_secret_static = {'p': 22265940615939518654421354186895036624144229614508843228257056207427890116167, 
+#                    'q': 111099633510718619897142263161279998876195698223355000056868443411384083193593, 
+#                    'd': 2223861520195205759233086909293430975747517740189242579374882797860657408583665005596333280605144797294794843233212631180363022087762053355600318904779025}
 
-message_static = 126
-print("n = ", hex(static_open_keys['n'])[2:].upper(), "\ne = ", bin(static_open_keys['e'])[2:])
-# print(static_secret_keys)
-encrypted = cipher_message(message_static, static_open_keys)
-encrypted_server = int('4253C72DDF34930D77E4672C701A23DB57CBAE97D4CD6ADD00310D9D96846792', 16)
-# print("encrypted message: ", hex(encrypted)[2:].upper())
-print("decrypted message:", decipher_message(encrypted_server, privateKeys=static_secret_keys))
+# B_open_static = {'n': 318364486092893208619427667853533833650943617879195340493482118970501846613364636276430456903206261414312196320388291127571713722901711520673894880680103, 
+#                  'e': 65537}
+# B_secret_static = {'p': 7797314612703150363262210733209742905963352618298642126655537251488120650741, 
+#                    'q': 40830016730916996314612890465226273697823794106448457431491205995781196864683, 
+#                    'd': 94396427876727662692718898358635113836537168052070187168612303520679797418099038494604992780457750626919550638923668787949070836700064860383511420165953}
+
+
+# static_secret_keys = {'p': 69819953805287766503459646966142998171613528092044328272563921438650723424849, 
+#                       'q': 59990584485103357398000765071984267951170634416743890283750519191068638236107, 
+#                       'd': 1888891693201938364012848145832313386426576551493966477173661553842167363314715505737594531118518480110207984568594845872784566495703169248672251115517793}
+# static_open_keys = {'n': 4188539837502129404916597155588303921713366518533584199442742590226835475877528223650275936240272484975824311826163400436055806808605328247100141332822843, 
+#                     'e': 65537}
+# print(static_secret_keys, static_open_keys)
+# message_static = 126
+
+# print("n = ", hex(static_open_keys['n'])[2:].upper(), "\ne = ", bin(static_open_keys['e'])[2:])
+
+# encrypted = cipher_message(message_static, static_open_keys)
+# encrypted_server = int('4253C72DDF34930D77E4672C701A23DB57CBAE97D4CD6ADD00310D9D96846792', 16)
+# print("encrypted message: ", encrypted)
+# print("decrypted message:", decipher_message(encrypted, privateKeys=static_secret_keys))
+# csign = crypto_sign(message_static, static_secret_keys)
+
+# print("crypto sign: ", csign)
+# print("verification: ", verification(message_static, csign, static_open_keys))
